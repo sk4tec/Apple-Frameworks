2d81f7dd722f651b8c9467686f557104b47d85cd
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2023-01-06 14:57:40 +0000

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Sunjay Kalsi on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
