77a4b760e67e2d7385a76babd6d5a8814ebe0791
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2023-01-09 16:17:33 +0000

    put mock data into a LazyVGrid

--- Apple Frameworks/Framework.swift
+++ Apple Frameworks/Framework.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct Framework {
+struct Framework: Hashable {
     let name: String
     let imageName: String
     let urlString: String
--- Apple Frameworks/FrameworkGridView.swift
+++ Apple Frameworks/FrameworkGridView.swift
@@ -1,13 +1,17 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible())]
+
     var body: some View {
-        ScrollView {
-            VStack {
-                Image("arkit")
-                    .resizable()
-                    .frame(width: 90, height: 90)
-                Text()
+        LazyVGrid(columns: columns) {
+
+            ForEach(MockData.frameworks, id: \.self) { framework in
+                //FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+                Text("Name: \(framework.name)")
+                Text("Image: \(framework.imageName)")
             }
         }
     }
@@ -16,5 +20,23 @@ struct FrameworkGridView: View {
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+    }
+}
+
+struct FrameworkTitleView: View {
+    let name: String
+    let imageName: String
+
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
     }
 }
